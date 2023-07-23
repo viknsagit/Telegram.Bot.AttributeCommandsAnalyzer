701c528d0df6e88e587035d58ddae7423498b084
viknsagit <viknsagitarius@gmail.com>
2023-07-23 23:46:31 +0500

    Добавьте файлы проекта.

---
+++ Telegram.Bot.AttributeCommandsAnalyzer.CodeFixes/TelegramBotAttributeCommandsAnalyzerCodeFixProvider.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Rename;
+using Microsoft.CodeAnalysis.Text;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Telegram.Bot.AttributeCommandsAnalyzer
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(TelegramBotAttributeCommandsAnalyzerCodeFixProvider)), Shared]
+    public class TelegramBotAttributeCommandsAnalyzerCodeFixProvider : CodeFixProvider
+    {
+        public override sealed ImmutableArray<string> FixableDiagnosticIds
+        {
+            get { return ImmutableArray.Create(TelegramBotAttributeCommandsAnalyzerAnalyzer.DiagnosticId); }
+        }
+
+        public override sealed FixAllProvider GetFixAllProvider()
+        {
+            // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public override sealed async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+            // TODO: Replace the following code with your own analysis, generating a CodeAction for each fix to suggest
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+            // Find the type declaration identified by the diagnostic.
+            var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<MethodDeclarationSyntax>().First();
+
+            // Register a code action that will invoke the fix.
+            context.RegisterCodeFix(
+                CodeAction.Create(
+                    title: CodeFixResources.CodeFixTitle,
+                    createChangedSolution: c => MakeMethodStaticAsync(context.Document, declaration, c),
+                    equivalenceKey: nameof(CodeFixResources.CodeFixTitle)),
+                diagnostic);
+        }
+
+        private async Task<Solution> MakeMethodStaticAsync(Document document, MethodDeclarationSyntax methodDeclaration, CancellationToken cts)
+        {
+            var root = await document.GetSyntaxRootAsync(cts).ConfigureAwait(false);
+
+            var newModifiers = methodDeclaration.Modifiers.Add(SyntaxFactory.Token(SyntaxKind.StaticKeyword));
+            var newMethodDeclaration = methodDeclaration.WithModifiers(newModifiers);
+
+            var newRoot = root.ReplaceNode(methodDeclaration, newMethodDeclaration);
+
+            return document.Project.Solution.WithDocumentSyntaxRoot(document.Id, newRoot);
+        }
+    }
+}
---
+++ Telegram.Bot.AttributeCommandsAnalyzer/TelegramBotAttributeCommandsAnalyzerAnalyzer.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+
+namespace Telegram.Bot.AttributeCommandsAnalyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class TelegramBotAttributeCommandsAnalyzerAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "TelegramBotAttributeCommandsAnalyzer";
+
+        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
+        // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Localizing%20Analyzers.md for more on localization
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private const string Category = "Naming";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+
+            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
+            // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
+            context.RegisterSyntaxNodeAction(AnalyzeMethod, SyntaxKind.MethodDeclaration);
+        }
+
+        private void AnalyzeMethod(SyntaxNodeAnalysisContext context)
+        {
+            var methodDeclaration = (MethodDeclarationSyntax)context.Node;
+
+            if (!methodDeclaration.Modifiers.Any(SyntaxKind.StaticKeyword))
+            {
+                if (methodDeclaration.AttributeLists.SelectMany(attrList => attrList.Attributes).Any(attr => attr.Name.ToString() == "TextCommand"))
+                {
+                    var diagnostic = Diagnostic.Create(Rule, methodDeclaration.GetLocation(), methodDeclaration.Identifier.Text, "TextCommand");
+                    context.ReportDiagnostic(diagnostic);
+                }
+                else if (methodDeclaration.AttributeLists.SelectMany(attrList => attrList.Attributes).Any(attr => attr.Name.ToString() == "CallbackCommand"))
+                {
+                    var diagnostic = Diagnostic.Create(Rule, methodDeclaration.GetLocation(), methodDeclaration.Identifier.Text, "CallbackCommand");
+                    context.ReportDiagnostic(diagnostic);
+                }
+            }
+        }
+    }
+}
